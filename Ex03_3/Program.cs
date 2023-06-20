dcc59aeea1835fdb7890a2d8f518a50f5d7770f9
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-06-20 19:12:51 +0500

    Задача 23

---
+++ Ex03_3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, 
+// которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+System.Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <=number; i++){
+    System.Console.Write(Math.Pow(i , 3)+ " ");
+}
